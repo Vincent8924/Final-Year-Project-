7270813f667609f40c1f9c8430b5020d2ff088bd
Vincent <1221202213@student.mmu.edu.my>
2024-05-24 13:16:49 +0800

    hi

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
             <br/><br/>
             <div class="center" ><a href="employer login.php"><span class="formBox">Employer</span></div>
             <br/><br/>
-            <div class="center" ><a href="../Admin/Alogin.php"><span class="formBox">Admin</span></div>
+            <div class="center" ><a href="Admin/Alogin.php"><span class="formBox">Admin</span></div>
 
 
 

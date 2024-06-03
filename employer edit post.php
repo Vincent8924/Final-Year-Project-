c8fe21a330efcac490d0da3687fa86f39685c008
GohChengHong <Hong@mmu.edu.my>
2024-06-03 17:32:39 +0800

    2

--- employer edit post.php
+++ employer edit post.php
@@ -6,8 +6,9 @@
         <title>
             Drafts | Job Help
         </title>
-        <link rel="stylesheet" type="text/css" href="employer home.css">
+        <link rel="stylesheet" type="text/css" href="employer edit post.css">
         <link rel="icon" href="img/logo.png">
+       
     </head>
     <body>
     <?php
@@ -21,40 +22,38 @@
                 echo'<script>alert("Log-Out successful!");window.location.href="employer login.php";</script>';
             }
                     ?>
-        <nav>
-            <div id="line">
-                <div class="choice">
-                    <span class="left">
-                    <a href="employer home.php"><img src="img/page logo2.png" id="page_logo"/></a>
-                    </span>    
-                    <span class="mid"> 
+        <header>
+    <div class="logo">
+        <a href="employer home.php"><img src="img/page logo2.png" id="page_logo"/></a>
+    </div>
+    <nav class="navigation">
+        <ul>
+            <li><a href="employer home.php">Home</a></li>
+            <li><a href="employer drafts.php">Drafts</a></li>
+            <li><a href="employer view post.php">Post</a></li>
+            <li><a href="employer view application.php">Application</a></li>
+            <li><a href="employer packages.php">Package</a></li>
+            <li><a href="employer payment history.php">History</a></li>
+            <li><a href="employer profile.php">Profile</a></li>
+        </ul>
+    </nav>
+    <form method="post" >
+       
+            <button id="logout" name="logout" onclick='return userconfirmation();'><img src='img/logout.png' id="logout_photo">LOG OUT</button>
+        </div> 
+    </form>
+</header>
 
-                    
-                    <a href="employer home.php">HOME</a>
-                        <a href="employer drafts.php">Drafts</a>
-                        <a href="employer view post.php">Post</a>
-                        <a href="employer view application.php">Application</a>
-                        <a href="employer packages.php">Package</a>
-                        <a href="employer payment history.php">History</a>
-                        <a href="employer profile.php">Profile</a>
-                    
-                    </span>
-                    <form method="post" >
-                        <span class="right" >
-                            <button id="logout" name="logout" onclick='return userconfirmation();'><img src='img/logout.png' id="logout_photo">LOG OUT</button>
-                        </span> 
-                    </form>
                 </div>
             </div>
         </nav>
 
+
   
     <br/><hr/><br/>
-
-    <h1>Edit your post</h1>
-        <br/><br/>
-        <form class="formBox" name="addnewpost" method="post">
-            
+    <div class="formBox">
+    <h1 style="text-align: center;">Edit your post</h1>
+    <form name="addnewpost" method="post">
                
         <?php
      
@@ -181,7 +180,7 @@
 
                 <h2>Description?</h2>
                 <input type="text" class="field" placeholder="<?php echo $row['description']?>" name="description" value="<?php echo $row['description']?>">
-                <br/><br/><hr/><br/><br/>
+             
                 <button type="submit" id="add" name="edit" value="Edit">Edit post</button>
 
                 <?php

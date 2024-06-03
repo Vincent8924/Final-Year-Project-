29c5e629530baa08e1d8189a4d0c36b6795bb821
GohChengHong <Hong@mmu.edu.my>
2024-06-03 18:12:00 +0800

    1

--- employer view application.php
+++ employer view application.php
@@ -6,8 +6,11 @@
         <title>
             Application | Job Help
         </title>
-        <link rel="stylesheet" type="text/css" href="employer home.css">
+        <link rel="stylesheet" type="text/css" href="employer view applcation.css">
         <link rel="icon" href="img/logo.png">
+        <style>
+
+        </style>
     </head>
     <body>
     <br/> <br/>
@@ -34,32 +37,32 @@
             }
 
                     ?>
-        <nav>
-            <div id="line">
-                <div class="choice">
-                    <span class="left">
-                        <a href="employer home.php"><img src="img/page logo2.png" id="page_logo"/></a>
-                    </span>    
-                    <span class="mid"> 
+        <header>
+    <div class="logo">
+        <a href="employer home.php"><img src="img/page logo2.png" id="page_logo"/></a>
+    </div>
+    <nav class="navigation">
+        <ul>
+            <li><a href="employer home.php">Home</a></li>
+            <li><a href="employer drafts.php">Drafts</a></li>
+            <li><a href="employer view post.php">Post</a></li>
+            <li><a href="employer view application.php">Application</a></li>
+            <li><a href="employer packages.php">Package</a></li>
+            <li><a href="employer payment history.php">History</a></li>
+            <li><a href="employer profile.php">Profile</a></li>
+        </ul>
+    </nav>
+    <form method="post" >
+       
+            <button id="logout" name="logout" onclick='return userconfirmation();'><img src='img/logout.png' id="logout_photo">LOG OUT</button>
+        </div> 
+    </form>
+</header>
 
-                    
-                    <a href="employer home.php">HOME</a>
-                        <a href="employer drafts.php">Drafts</a>
-                        <a href="employer view post.php">Post</a>
-                        <a href="employer view application.php">Application</a>
-                        <a href="employer packages.php">Package</a>
-                        <a href="employer payment history.php">History</a>
-                        <a href="employer profile.php">Profile</a>
-                    
-                    </span>
-                    <form method="post" >
-                        <span class="right" >
-                            <button id="logout" name="logout" onclick='return userconfirmation();'><img src='img/logout.png' id="logout_photo">LOG OUT</button>
-                        </span> 
-                    </form>
                 </div>
             </div>
         </nav>
+
 
     
     <br/><hr/><br/>

a9f224f657fb4d07d4c83bb70bf876357c4f0c1b
GohChengHong <Hong@mmu.edu.my>
2024-06-19 12:21:12 +0800

    2

--- jobseeker index.php
+++ jobseeker index.php
@@ -9,13 +9,28 @@
    
 </head>
 <body>
-    <header class="header">
-        <img src="general_image/jobhelper_logo.png" class="logo">
-        <div>
-            <a href="register.php">Register</a>
-            <a href="login.php">Log In</a>
+
+<header>
+        <div class="logo">
+        <img src="../Final-Year-Project-/general_image/jobhelper_logo.png" alt="JobStreet Logo">
+        </div>
+        <nav class="navigation">
+            <ul>
+            <li><a href="homepage.php?email=<?php echo urlencode($_SESSION['id']); ?>">Homepage</a></li>  
+                <li><a href="jobsave.php?email=<?php echo urlencode($_SESSION['id']); ?>">Job Save</a></li>
+                <li><a href="profile.php?email=<?php echo urlencode($_SESSION['id']); ?>">Profile</a></li>
+                <li><a href="applylist.php?email=<?php echo urlencode($_SESSION['id']); ?>">Apply list</a></li>
+   
+   
+            </ul>
+        </nav>
+        </div>
+
+        <div class="employer-site">
+            <a href="employer sign up.php">Employer Site</a>
         </div>
     </header>
+
     
     <div id="slideshow" class="index_container" >
         <img src="lo.jpg" class="active">

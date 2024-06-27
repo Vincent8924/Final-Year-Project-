0157fb8902eacba976132854b400cbf8a4af69f1
GohChengHong <Hong@mmu.edu.my>
2024-06-28 00:10:39 +0800

    1

--- applylist.php
+++ applylist.php
@@ -31,7 +31,7 @@ $result = $connect->query($query);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   
+    <link rel="stylesheet" type="text/css" href="applylist.css">
     <title>Your Page Title</title>
 
 </head>

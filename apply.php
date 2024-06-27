dee7186b1c3ff0a5c5ea1f475ebec776f3fc2ad8
GohChengHong <Hong@mmu.edu.my>
2024-06-28 00:07:47 +0800

    4

--- apply.php
+++ apply.php
@@ -104,6 +104,7 @@ if(isset($_GET['post_id'])) {
             <li><a href="homepage.php?email=<?php echo urlencode($_SESSION['id']); ?>">Homepage</a></li>
             <li><a href="jobsave.php?email=<?php echo urlencode($_SESSION['id']); ?>">Job save</a></li>
             <li><a href="profile.php?email=<?php echo urlencode($_SESSION['id']); ?>">Profile</a></li>
+            <li><a href="applylist.php?email=<?php echo urlencode($_SESSION['id']); ?>">Apply list</a></li>
               
             </ul>
         </nav>
@@ -146,7 +147,7 @@ if(isset($_GET['post_id'])) {
             <ul>
             <li><a href="aboutus.php?email=<?php echo urlencode($_SESSION['id']); ?>">About us</a></li>
             <li><a href="contact.php?email=<?php echo urlencode($_SESSION['id']); ?>">Contact us</a></li>
-            <li><a href="applylist.php?email=<?php echo urlencode($_SESSION['id']); ?>">Apply list</a></li>
+           
                 
             </ul>
         </nav>

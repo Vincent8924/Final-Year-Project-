cbcfcc09f485433f353d06295188143f6d296974
GohChengHong <Hong@mmu.edu.my>
2024-06-28 00:08:50 +0800

    4

--- applylist.php
+++ applylist.php
@@ -31,8 +31,155 @@ $result = $connect->query($query);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="applylist.css">
+   
     <title>Your Page Title</title>
+
+    <style>
+        body {
+    background-color: white; 
+}   
+
+header {
+    background-color: white;
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.navigation {
+    display: inline-block;
+    margin-right: 40%;
+}
+
+.navigation ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+}
+
+.navigation ul li {
+    display: inline-block;
+    margin-right: 20px;
+}
+
+.navigation ul li a {
+    text-decoration: none;
+    color: #333;
+    font-weight: bold;
+    transition: color 0.3s;
+}
+
+.navigation ul li a:hover {
+    color: #555;
+}
+
+.employer-site {
+    display: inline-block;
+    padding: 8px 16px;
+    border: 2px solid blue;
+    border-radius: 5px;
+    margin-left: 20px;
+}
+
+.employer-site a {
+    text-decoration: none;
+    color: rgb(12, 12, 191);
+}
+
+.employer-site:hover {
+    background-color: blue;
+}
+
+.employer-site:hover a {
+    color: white;
+}
+
+.logo {
+    display: inline-block;
+}
+
+.logo img {
+    height: 50px;
+}
+
+.user-info {
+    display: inline-block;
+    padding: 8px 16px;
+    border: 2px solid green;
+    border-radius: 5px;
+    margin-left: 50px;
+}
+
+.user-info p {
+    margin: 0;
+    font-weight: bold;
+    color: green;
+}
+
+.user-info:hover {
+    background-color: green;
+}
+
+.user-info:hover p {
+    color: white;
+}
+
+table {
+border-collapse: collapse;
+width: 100%;
+}
+
+th, td {
+padding: 8px;
+text-align: left;
+}
+
+thead {
+background-color: #f2f2f2;
+}
+
+th {
+background-color: #ddd;
+font-weight: bold;
+}
+
+tbody tr:nth-child(even) {
+background-color: #f2f2f2;
+}
+
+footer {
+    background-color: white;
+    padding: 10px 20px;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    box-shadow: 0px -1px 10px rgba(0, 0, 0, 0.1);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+footer nav ul {
+    font-family: 'Times New Roman', Times, serif;
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    gap: 20px;
+}
+
+footer nav ul li a {
+    text-decoration: none;
+    color: #333;
+    font-weight: bold;
+    transition: color 0.3s;
+}
+
+footer nav ul li a:hover {
+    color: #555;
+}
+    </style>
 </head>
 <body>
 
